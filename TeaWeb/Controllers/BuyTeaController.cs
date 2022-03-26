8ddad48c2b8b9c25142d12933e4a2d66ece0dd55
Leo <leo12160201@gmail.com>
2022-03-26 20:49:10 +0800

    V1

---
+++ TeaWeb/Controllers/BuyTeaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeaWeb.Models;
+
+namespace TeaWeb.Controllers
+{
+  public class BuyTeaController : Controller
+  {
+    // GET: BuyEat
+    public ActionResult BuyList()
+    {
+      List<Item> itemList = new List<Item>();
+      Item item1 = new Item { Id = 0, Name = "Item1", Image = "/Image/logo.jpg" };
+      itemList.Add(item1);
+      return View(itemList);
+    }
+  }
+}
